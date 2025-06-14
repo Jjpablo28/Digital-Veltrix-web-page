849b152209f2fc64cc703216690b0b57f4a7430f
JuanDavidG31 <jdgonzalezh@unbosque.edu.co>
2025-06-14 16:19:51 -0500

    Genereing back

---
+++ backdigitalvertix/src/main/java/com/digital/vertix/backdigitalvertix/dto/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Paquete que contiene las clases de Transferencia de Datos (DTOs) utilizadas
+ * en la aplicación digitalvertix backend.
+ */
+package com.digital.vertix.backdigitalvertix.dto;
