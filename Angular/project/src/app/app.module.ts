ac007903b96cae8aa57f8b92726ec0d58fbe4008
Jjpablo28 <jpr.rod06@gmail.com>
2025-06-14 14:32:43 -0500

    Nuevo front

--- Angular/project/src/app/app.module.ts
+++ Angular/project/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NavbarComponent} from "./navbar/navbar.component";
 import {FooterComponent} from "./footer/footer.component";
-import { MainComponent } from './main/main.component';
+import {MainComponent} from "./main/main.component";
 
 @NgModule({
   declarations: [
