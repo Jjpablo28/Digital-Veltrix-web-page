23875e88ffafef6ea240f401fb8e05863aeb8036
Jjpablo28 <jpr.rod06@gmail.com>
2026-04-13 23:52:33 -0500

    canonical

--- Angular/project/src/app/app.component.ts
+++ Angular/project/src/app/app.component.ts
@@ -1,19 +1,45 @@
-import {Component, HostListener} from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   mostrarScrollTop: boolean = false;
   title = 'Digital Veltrix';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.setCanonicalURL();
+      }
+    });
+  }
+
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-
   }
+
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.mostrarScrollTop = window.scrollY > 200;
+  }
+
+  setCanonicalURL() {
+    const url = 'https://digital-veltrix.netlify.app' + this.router.url;
+
+    let link: HTMLLinkElement =
+      document.querySelector("link[rel='canonical']") ||
+      document.createElement('link');
+
+    link.setAttribute('rel', 'canonical');
+    link.setAttribute('href', url);
+
+    document.head.appendChild(link);
   }
 }
